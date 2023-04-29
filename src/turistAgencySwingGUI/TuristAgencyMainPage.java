29da8155adf063bc80c8408527c6f8753725b8b8
radicSR16 <radic.sr16.2022@uns.ac.rs>
2023-04-29 18:33:12 +0200

    commit message

---
+++ src/turistAgencySwingGUI/TuristAgencyMainPage.java
@@ -0,0 +1,43 @@
+package turistAgencySwingGUI;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class TuristAgencyMainPage {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TuristAgencyMainPage window = new TuristAgencyMainPage();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public TuristAgencyMainPage() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
